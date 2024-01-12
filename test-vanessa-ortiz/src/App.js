4e0e66fa64e79d8868e872ed8f6a06bb38f472c3
VanessaMOA <132621860+VanessaMOA@users.noreply.github.com>
2024-01-11 21:01:12 -0600

    Setup

---
+++ test-vanessa-ortiz/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import AutoComplete from './components/prueba';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <AutoComplete/>
+        <AutoComplete>
+          
+        </AutoComplete>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ test-vanessa-ortiz/src/components/prueba.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function AutoComplete({ isVisible, suggestions, handleSuggestionClick }) {
+  return (
+    <div className="flex flex-wrap">
+      <h1 className='w-100% '>
+            i
+      </h1>
+      <h1 >
+            i
+      </h1>
+      <h1>
+            i
+      </h1>
+    </div>
+  );
+}
+
+export default AutoComplete;
